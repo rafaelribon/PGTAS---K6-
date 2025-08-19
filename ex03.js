1056dbcdcff3ac2f85da81acda900c3a065b2181
Rafael Grassi Ribon <rafael.ribon@gpsafira.com.br>
2025-08-19 14:32:18 -0300

    Instalando o Faker

--- ex03.js
+++ ex03.js
@@ -1,69 +1,65 @@
-import http, { head } from 'k6/http';
-import { sleep, check } from 'k6';
+import http from 'k6/http'
+import { sleep, check, group } from 'k6'
+import { faker } from 'https://esm.sh/@faker-js/faker'
 
+export default function() {
+    let tokenColetadoNaResposta;
 
-export default function () {
+    group('Obtendo o Token do Usuário', () => {
+        const endpointRequestLogin = 'http://165.227.93.41/lojinha/v2/login'
 
-    const endpointRequestLogin = 'http://165.227.93.41/lojinha/v2/login'
+        const bodyRequestLogin = JSON.stringify({
+            usuarioLogin: 'cgts',
+            usuarioSenha: '123456'
+        })
 
-    const bodyRequestLogin = JSON.stringify({
-        usuarioLogin: 'cgts',
-        usuarioSenha: '123456'
+        const optionsRequestLogin = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        } 
+
+        const respostaLogin = http.post(endpointRequestLogin, bodyRequestLogin, optionsRequestLogin)
+
+        check(respostaLogin, {
+            'Status code é igual a 200': r => r.status === 200,
+            'A mensagem de sucesso ao realizar o login foi apresentada': r => r.json('message') === 'Sucesso ao realizar o login'
+        })
+
+        // Extraiam o token da resposta usando GJson
+        tokenColetadoNaResposta = respostaLogin.json('data.token')
     })
 
-    const optionsRequestLogin = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-    const respostaLogin = http.post(endpointRequestLogin, bodyRequestLogin, optionsRequestLogin)
-    check(respostaLogin, {
-        'Status Code é  igual  a 200': r => r.status === 200,
-        'A mensagem de sucesso ao realizar o login foi apresentada': r => r.json().message === 'Sucesso ao realizar o login',
-        'A mensagem de sucesso ao realizar o login foi apresentada': r => r.json('message') === 'Sucesso ao realizar o login'
-    });
+    group('Cadastrar um Novo Produto', () => {
+        // Cadastrem um produto
+        const respostaCadastroProduto = http.post(
+            'http://165.227.93.41/lojinha/v2/produtos',
+            JSON.stringify({
+                produtoNome: faker.commerce.productName(),
+                produtoValor: 50,
+                produtoCores: [ 'preto', 'branco' ]
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    token: tokenColetadoNaResposta
+                }
+            }
+        )
 
-    //Extraiam o token da resposta usando GJson
-    const token = respostaLogin.json('data.token');
-
-
-    //Cadastrem um produto
-    const endpointCadastroProduto = 'http://165.227.93.41/lojinha/v2/produtos'
- 
-
-    const bodyCadastroProduto = JSON.stringify({
-        "produtoNome": "Teste Abacate ",
-        "produtoValor": 50,
-        "produtoCores": ["amarelo", "verde"]
+        // Checks
+        // - Status Code
+        // - Mensagem
+        // - Sgunda Cor do Produto
+        check(respostaCadastroProduto, {
+            'Status Code é 201': r => r.status === 201,
+            'Mensagem é produto adicionado com sucesso': r => r.json().message === 'Produto adicionado com sucesso',
+            'Segunda cor do produto é branco': r => r.json('data.produtoCores.1') === 'branco',
+            'Segunda cor do produto é branco (sem GJSON)': r => r.json().data.produtoCores[1] === 'branco',
+        })
     })
 
-     const optionsCadastroProduto = {
-        headers: {
-            'Content-Type': 'application/json',
-            token: token
-        }
-    }
-       const respostaCadastroProduto = http.post(endpointCadastroProduto, bodyCadastroProduto, optionsCadastroProduto )
-
-       //Checks
-       //- Status Code 
-       //- Mensagem 
-       // - Segunda Cor do Produto 
-
-       check(respostaCadastroProduto, {
-        'Status Code é igual a 201': r => r.status === 201,
-        'Mensagem é produto adicionado com sucesso': r => r.json('message') === 'Produto adicionado com sucesso',
-        'Segunda cor do produto é verde': r => r.json('data.produtoCores.1')=== 'verde',
-        'Segunda cor do produto é verde (Sem GJSON)': r => r.json().data.produtoCores[1] === 'verde'
-
-
-       })
-
-
-
-    sleep(1) // Entenda o comportamento e quem  e seu usuário 
-
+    group('User Think Time Após o Cadastro', () => {
+        sleep(1) // Entenda o comportamento e quem é seu usuário
+    })
 }
-
-
-
